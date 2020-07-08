eb96eefdcb34f9a3f9c1493aef68fd1b8aaabab3
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-07-08 22:50:38 +0300

    Create header part

---
+++ Client/spotify/src/Header/Navigation.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import {Navbar, Form, Button, FormControl} from "react-bootstrap/";
+import styles  from './navigation.module.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <Navbar expand="lg" className={styles.navigation}>
+        <Navbar.Brand href="/">
+          <span className={styles.logo}>Spotify-App</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Form inline className='m-auto'>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default Header;

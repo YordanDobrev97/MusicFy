2bf000c5d5d476318974bfce4830a5b98e21b53e
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-07-09 17:36:13 +0300

    Add Router between pages

--- Client/spotify/src/Header/Sidebar.js
+++ Client/spotify/src/Header/Sidebar.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
+import { Route, NavLink, HashRouter } from 'react-router-dom'
+import Home from '../Home';
+import Songs from '../Songs';
+import Playlist from '../Playlist';
+import Login from '../Login';
+import Register from '../Register';
+
 import styles from './sidebar.module.css';
 
 class SideBarComponent extends Component {
     render() {
         return (
-            <div className={styles.sidenav}>
-                <a href="/home">Home</a>
-                <a href="/songs">Songs</a>
-                <a href="/playlist">Playlist</a>
-                <a href="/login">Login</a>
-                <a href="/register">Register</a>
-          </div>
+            <HashRouter>
+                <div className={styles.sidenav}>
+                    <ul>
+                        <li>
+                            <NavLink to='/home'>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/songs'>Songs</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/playlist'>Playlist</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/login'>Login</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/register'>Register</NavLink>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <Route path='/home' component={Home} />
+                    <Route path='/songs' component={Songs} />
+                    <Route path='/playlist' component={Playlist} />
+                    <Route path='/login' component={Login} />
+                    <Route path='/register' component={Register} />
+                    </div>
+            </HashRouter>
         )
     }
 }

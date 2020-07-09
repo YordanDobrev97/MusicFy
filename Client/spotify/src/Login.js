28821f39d529391977bea657b15e90acb24d1eb9
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-07-09 22:39:04 +0300

    Add login form

--- Client/spotify/src/Login.js
+++ Client/spotify/src/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 function Login() {
     return (
         <div>
-            <h1>Login...</h1>
+           <MuiThemeProvider>
+               <div>
+                   <AppBar title='Login'/>
+                   <TextField hintText='Your username' floatingLabelText='Username' />
+                   <br />
+                   <TextField hintText='Your password' floatingLabelText='Password'/>
+                    <br />
+                   <RaisedButton label='Submit' primary={true}/>
+               </div>
+           </MuiThemeProvider>
         </div>
     )
 }

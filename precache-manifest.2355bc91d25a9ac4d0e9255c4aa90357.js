2bce1bdbf8b226a3072bae4ec00627c23d4c593f
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-07-24 01:01:40 +0300

    Updates

---
+++ precache-manifest.2355bc91d25a9ac4d0e9255c4aa90357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75f22cb8d5b6e26ddc5230de058227e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a43eeadefb765046c17",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3f4188dba32f7aa611ae",
+    "url": "/static/css/main.343b285d.chunk.css"
+  },
+  {
+    "revision": "7a43eeadefb765046c17",
+    "url": "/static/js/2.7941a555.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "/static/js/2.7941a555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f4188dba32f7aa611ae",
+    "url": "/static/js/main.aed79221.chunk.js"
+  },
+  {
+    "revision": "618a94e805e6ab69a130",
+    "url": "/static/js/runtime-main.d41ee8e9.js"
+  }
+]);

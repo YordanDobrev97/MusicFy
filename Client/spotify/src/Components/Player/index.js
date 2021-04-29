77a0f1affa798452e47d240f7dce0d2ee38c8644
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2021-04-29 03:21:47 +0300

    Add timing on the player

--- Client/spotify/src/Components/Player/index.js
+++ Client/spotify/src/Components/Player/index.js
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
 import React, { useState, useEffect, useContext } from "react";
-=======
-import React, { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBackward,
-  faFastBackward,
-  faPauseCircle,
-  faPlayCircle,
-  faForward,
-  faFastForward,
-} from "@fortawesome/free-solid-svg-icons";
->>>>>>> 2ba9e3de5e392c116f124859f21b2affa7b76e9d
-
 import "./player.css";
 import SongContext from '../../SongContext'
 
@@ -20,56 +6,65 @@ const Player = (props) => {
   const context = useContext(SongContext)
   const [song, setPlay] = useState(context[0]);
   const [time, setTime] = useState("0")
-  const [currentTime, setCurrentTime] = useState("0")
+  const [seconds, setSeconds] = useState(0)
+  const [minutes, setMinutes] = useState(0)
   const [songName, setName] = useState("")
+  const [finish, setFinish] = useState(false)
 
   useEffect(() => {
     const {link, time, name} = context[0];
-    console.log(link);
-    setPlay(link + '?autoplay=1')
-    setTime(time)
-    setName(name)
-  }, [context]);
+    if (name) {
+      setPlay(link + '?autoplay=1')
+      setTime(time)
+      setName(name)
+    }
 
+    if (time === `${minutes}:${seconds}`) {
+      setFinish(true)
+    }
+
+    if (finish) {
+      setSeconds(0)
+      setMinutes(0)
+      setTime("0")
+      setPlay("")
+      setFinish(true)
+    }
+
+    let interval;
+    if (songName) {
+        interval = setInterval(() => {
+        const newTime = seconds + 1
+
+        if (newTime > 59) {
+            setSeconds(0)
+            setMinutes(minutes + 1)
+        } else {
+            setSeconds(newTime)
+        }
+      }, 1000)
+    }
+
+    return () => clearInterval(interval)
+  }, [context, seconds, songName, finish]);
+
+  const timing = seconds < 10 ? `${minutes}:0${seconds}` : `${minutes}:${seconds}`
   return (
-<<<<<<< HEAD
     <div class="container-audio">
         <h1>You are currently listening: {songName}</h1>
-        <p>Time: {time}</p>
-        {song ? (<React.Fragment></React.Fragment>) : (<button>Pause</button>)}
-=======
     <div className="player-container">
       <div className="btn-group">
-        <button type="button" className="btn btn-dark">
-          <FontAwesomeIcon icon={faFastBackward} style={{ color: "grey" }} />
-        </button>
-        <button type="button" className="btn btn-dark">
-          <FontAwesomeIcon icon={faBackward} style={{ color: "grey" }} />
-        </button>
-        <button type="button" className="btn btn-dark">
-          <FontAwesomeIcon icon={faPauseCircle} style={{ color: "grey" }} />
-        </button>
-        <button type="button" className="btn btn-dark">
-          <FontAwesomeIcon icon={faPlayCircle} style={{ color: "grey" }} />
-        </button>
-        <button type="button" className="btn btn-dark">
-          <FontAwesomeIcon icon={faForward} style={{ color: "grey" }} />
-        </button>
-        <button type="button" className="btn btn-dark">
-          <FontAwesomeIcon icon={faFastBackward} style={{ color: "grey" }} />
-        </button>
       </div>
       <div class="main-control">
         <div className="btn _timeline">
-          <span className="current-time">00:00</span>
+          <span className="current-time">{timing}</span>
           <span className="timescope">
             <span className="timescope-dot"></span>
           </span>
-          <span className="end-time">4:00</span>
+          <span className="end-time">{time}</span>
         </div>
       </div>
       <div className="video">
->>>>>>> 2ba9e3de5e392c116f124859f21b2affa7b76e9d
         <iframe
           width="560"
           height="315"
@@ -78,6 +73,8 @@ const Player = (props) => {
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
       ></iframe>
+      </div>
+    </div>
     </div>
   );
 };

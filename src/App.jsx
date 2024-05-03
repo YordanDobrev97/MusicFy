8272ed8ba0ca2503ad1c516f2020b87d9143f54d
YordanDobrev <yordan.19Dobrev@gmail.com>
2024-05-03 21:32:08 +0300

    feat: add appbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-export default function App() {
+import { AppBar, IconButton, Toolbar, ThemeProvider, Typography } from '@mui/material'
+import { Menu } from '@mui/icons-material'
+import { createTheme } from '@mui/material/styles'
+
+import './index.css'
+
+const appTheme = createTheme({
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        colorPrimary: {
+          backgroundColor: "white"
+        }
+      }
+    }
+  }
+})
+
+const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <ThemeProvider theme={appTheme}>
+      <AppBar>
+        <Toolbar>
+          <IconButton>
+              <Menu />
+              <Typography variant='h6'>MusicFy</Typography>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
   )
 }
+
+export default App
